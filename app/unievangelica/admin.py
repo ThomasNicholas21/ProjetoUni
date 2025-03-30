e658823ecafd0f7d47ca210b7c02a593c80e45b3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-30 19:46:18 -0300

    feat: implementando admin do model sala

--- app/unievangelica/admin.py
+++ app/unievangelica/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Bloco
+from .models import Bloco, Sala
 
 # Register your models here.
 
 @admin.register(Bloco)
 class BlocoAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
     ...
