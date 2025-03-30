a9c4202cf69856ec9d9aaa0a6c67bdb7acbf4335
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-30 18:23:08 -0300

    feat: implementando model bloco e sua migração

--- app/unievangelica/models.py
+++ app/unievangelica/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Bloco(models.Model):
+    nome_bloco = models.CharField(max_length=20, blank=False)
+    # sala = models.ForeignKey('Sala', on_delete=models.CASCADE)
