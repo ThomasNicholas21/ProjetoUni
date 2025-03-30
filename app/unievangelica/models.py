de7c1e2ef9a49563d23fc6b445f21cc599eb8891
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-30 19:46:07 -0300

    feat: implementando model Sala

--- app/unievangelica/models.py
+++ app/unievangelica/models.py
@@ -4,4 +4,31 @@ from django.db import models
 
 class Bloco(models.Model):
     nome_bloco = models.CharField(max_length=20, blank=False)
-    # sala = models.ForeignKey('Sala', on_delete=models.CASCADE)
+
+
+class Sala(models.Model):
+    nome_sala = models.CharField(
+        max_length=20,
+        verbose_name="Nome da Sala",
+        help_text="Informe o nome/identificação da sala"
+    )
+    bloco = models.ForeignKey(
+        Bloco,
+        on_delete=models.PROTECT,
+    )
+    quantidade_maxima = models.PositiveIntegerField(
+        default=1,
+        blank=False,
+        verbose_name="Capacidade Máxima",
+        help_text="Quantidade máxima de pessoas permitida na sala"
+    )
+    status = models.CharField(
+        max_length=7,
+        choices=(
+        ('PUBLIC', 'Pública'),
+        ('PRIVATE', 'Privada'),
+        ),
+        default='PUBLIC',
+    )
+    # curso = models.ForeignKey('Curso', on_delete=models.SET_NULL, null=True)
+    # recursos_sala = models.ForeignKey('RecursosSala', on_delete=models.CASCADE)
