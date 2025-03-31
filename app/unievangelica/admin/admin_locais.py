70aa5e10176abd6052cdb92d97e1f4e7e9a136fe
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-31 14:09:30 -0300

    feat: implementando widget para o campo recurso sala

--- app/unievangelica/admin/admin_locais.py
+++ app/unievangelica/admin/admin_locais.py
@@ -10,9 +10,11 @@ class BlocoAdmin(admin.ModelAdmin):
 
 @admin.register(RecursoSala)
 class RecursoSalaAdmin(admin.ModelAdmin):
-    ...
+    search_fields = 'nome_recurso',
 
 
 @admin.register(Sala)
 class SalaAdmin(admin.ModelAdmin):
-    ...
+    autocomplete_fields = 'recursos_sala',
+    search_fields = 'nome_sala',
+
