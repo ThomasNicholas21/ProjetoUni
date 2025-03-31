42357ca89cad35bf7dca2cf71b769a322b353dee
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-31 14:18:50 -0300

    feat: implementando endpoint de detail

--- app/unievangelica/api/locais.py
+++ app/unievangelica/api/locais.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from ..serializer.locais import SerializerBloco
 from ..models.models_locais import Bloco
 
+
 @api_view(http_method_names=["get"])
 def api_get_blocos(request):
     if request.method == "GET":
@@ -11,6 +12,19 @@ def api_get_blocos(request):
         serializer = SerializerBloco(
             instance=blocos, 
             many=True
+            )
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(http_method_names=["get"])
+def api_get_bloco_detail(request, id):
+    if request.method == "GET":
+        bloco = Bloco.objects.filter(pk=id).first()
+        serializer = SerializerBloco(
+            instance=bloco, 
+            many=False
             )
         return Response(serializer.data)
     
