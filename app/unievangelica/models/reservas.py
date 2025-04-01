089b2ec135c3df68b8be69d7b53657db61b3cd49
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-04-01 16:28:09 -0300

    refactor: refatorando recorrencia dentro de reserva.

--- app/unievangelica/models/reservas.py
+++ app/unievangelica/models/reservas.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
-from django.core.exceptions import ValidationError
-import calendar
+
 
 # Create your models here.
 
@@ -23,97 +21,42 @@ class Reservas(models.Model):
     class Meta:
         verbose_name = 'Reserva'
         verbose_name_plural = 'Reservas'
-        ordering = ['data_inicio']
     
-    bloco = models.ForeignKey('Bloco', on_delete=models.PROTECT)
-    sala = models.ForeignKey('Sala', on_delete=models.PROTECT)
-    data_inicio = models.DateTimeField()
-    data_final = models.DateTimeField()
-    coordenador = models.ForeignKey(User, on_delete=models.PROTECT)
-    motivo_reserva = models.TextField()
-    curso = models.ForeignKey('Cursos', on_delete=models.PROTECT, null=True, blank=True)
-    recorrencia = models.CharField(
-        max_length=10,
-        choices=(
-        ('NENHUMA', 'Nenhuma recorrência'),
-        ('DIARIA', 'Diária'),
-        ('SEMANAL', 'Semanal'),
-        ('MENSAL', 'Mensal'),
-        ),
-        default='NENHUMA',
-        verbose_name='Tipo de recorrência'
+    nome_reserva = models.CharField(max_length=128, default='')
+    bloco = models.ForeignKey(
+        'Bloco', on_delete=models.PROTECT
     )
-    recorrencia_fim = models.DateTimeField(
-        null=True,
-        blank=True,
-        verbose_name='Data final da recorrência'
+    sala = models.ForeignKey(
+        'Sala', on_delete=models.PROTECT
+    )
+    data_reserva = models.DateTimeField()
+    horario_inicio = models.TimeField()
+    horario_final = models.TimeField()
+    coordenador = models.ForeignKey(
+        User, on_delete=models.PROTECT
+    )
+    motivo_reserva = models.TextField()
+    curso = models.ForeignKey(
+        'Cursos', 
+        on_delete=models.PROTECT, 
+        null=True, blank=True
+    )
+    recorrencia = models.CharField(
+        choices=(
+            ("N", "Sem recorrência"),
+            ("D", "Diária"),
+            ("S", "Semanal"),
+            ("M", "Mensal"),
+        ),
+        default="N",
+    )
+    quantidade_recorrencia = models.PositiveIntegerField(
+        null=True, blank=True
+    )
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="recorrencias"
     )
 
     def __str__(self):
-        return f"Reserva {self.sala} - {self.data_inicio.strftime('%d/%m/%Y %H:%M')}"
-    
-    def clean(self):
-        if self.data_inicio >= self.data_final:
-            raise ValidationError("A data final deve ser após a data de início")
-            
-        if self.recorrencia != 'NENHUMA' and not self.recorrencia_fim:
-            raise ValidationError("Para reservas recorrentes, informe a data final da recorrência")
-        
-        if self.sala.status == 'PRIVATE' and self.curso != self.sala.curso:
-            raise ValidationError("Esta sala é privada e só pode ser reservada pelo curso proprietário")
+        return f'{self.nome_reserva} para {self.curso.nome_curso} feito por {self.coordenador.first_name}'
 
-    @property
-    def ocorrencias_da_recorrencia(self):
-        if self.recorrencia == 'NENHUMA':
-            return [(self.data_inicio, self.data_final)]
-            
-        ocorrencias = []
-        
-        while self.data_inicio <= self.recorrencia_fim:
-            ocorrencias.append((self.data_inicio, self.data_final))
-            
-            if self.recorrencia == 'DIARIA':
-                self.data_inicio += timezone.timedelta(days=1)
-                self.data_final += timezone.timedelta(days=1)
-
-            elif self.recorrencia == 'SEMANAL':
-                self.data_inicio += timezone.timedelta(weeks=1)
-                self.data_final += timezone.timedelta(weeks=1)
-
-            elif self.recorrencia == 'MENSAL':
-                try:
-                    self.data_inicio = self.data_inicio.replace(month=self.data_inicio.month + 1)
-                    self.data_final = self.data_final.replace(month=self.data_final.month + 1)
-                    
-                except ValueError:
-                    if self.data_inicio.month == 12:
-                        self.data_inicio = self.data_inicio.replace(year=self.data_inicio.year + 1, month=1)
-                        self.data_final = self.data_final.replace(year=self.data_final.year + 1, month=1)
-
-                    else:
-                        proximo_mes = self.data_inicio.month + 1
-                        proximo_ano = self.data_inicio.year
-                        
-                        if proximo_mes > 12:
-                            proximo_mes = 1
-                            proximo_ano += 1
-                        
-                        ultimo_dia_original = calendar.monthrange(self.data_inicio.year, self.data_inicio.month)[1]
-
-                        if self.data_inicio.day == ultimo_dia_original:
-                            ultimo_dia_proximo = calendar.monthrange(proximo_ano, proximo_mes)[1]
-                            self.data_inicio = self.data_inicio.replace(year=proximo_ano, month=proximo_mes, day=ultimo_dia_proximo)
-                            self.data_final = self.data_final.replace(year=proximo_ano, month=proximo_mes, day=ultimo_dia_proximo)
-
-                        else:
-                            try:
-                                self.data_inicio = self.data_inicio.replace(year=proximo_ano, month=proximo_mes)
-                                self.data_final = self.data_final.replace(year=proximo_ano, month=proximo_mes)
-
-                            except ValueError:
-                                ultimo_dia_proximo = calendar.monthrange(proximo_ano, proximo_mes)[1]
-                                self.data_inicio = self.data_inicio.replace(year=proximo_ano, month=proximo_mes, day=ultimo_dia_proximo)
-                                self.data_final = self.data_final.replace(year=proximo_ano, month=proximo_mes, day=ultimo_dia_proximo)
-
-        return ocorrencias
-    
