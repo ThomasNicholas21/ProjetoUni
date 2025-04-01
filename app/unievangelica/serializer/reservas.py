6715d535f5eca5c7521f11231eb9827d8b2f5390
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-31 22:29:34 -0300

    feat: implementando serializador para User

--- app/unievangelica/serializer/reservas.py
+++ app/unievangelica/serializer/reservas.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 from unievangelica.models import Cursos
+from django.contrib.auth.models import User
+
+
+class SerializerUsuario(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username', 'email', 'password'
+        ]
+        extra_kwargs = {'password': {'write_only': True}}
 
 
 class SeralizerCursos(serializers.ModelSerializer):
