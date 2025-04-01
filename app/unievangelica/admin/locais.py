2b155238d112da95610c06cf6caa5ed6f7a5a220
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-04-01 20:55:53 -0300

    feat: personalizando admin

--- app/unievangelica/admin/locais.py
+++ app/unievangelica/admin/locais.py
@@ -5,16 +5,24 @@ from unievangelica.models import Bloco, Sala, RecursoSala
 
 @admin.register(Bloco)
 class BlocoAdmin(admin.ModelAdmin):
-    ...
+    list_display = ("id" ,"nome_bloco",)
+    search_fields = ("nome_bloco",)
 
 
 @admin.register(RecursoSala)
 class RecursoSalaAdmin(admin.ModelAdmin):
-    search_fields = 'nome_recurso',
+    list_display = ("id" ,"nome_recurso",)
+    search_fields = ("nome_recurso",)
 
 
 @admin.register(Sala)
 class SalaAdmin(admin.ModelAdmin):
-    autocomplete_fields = 'recursos_sala',
-    search_fields = 'nome_sala',
+    list_display = ("id" ,"nome_sala", "bloco", "quantidade_maxima", "status")
+    list_filter = ("bloco", "status")
+    search_fields = ("nome_sala",)
+    autocomplete_fields = ("recursos_sala", "curso")
+    fieldsets = (
+        ("Informações Gerais", {"fields": ("nome_sala", "bloco", "quantidade_maxima", "status")}),
+        ("Recursos e Curso", {"fields": ("recursos_sala", "curso")}),
+    )
 
--- app/unievangelica/admin/reservas.py
+++ app/unievangelica/admin/reservas.py
@@ -3,10 +3,19 @@ from unievangelica.models import Cursos, Reservas
 
 @admin.register(Cursos)
 class AdminCursos(admin.ModelAdmin):
-    autocomplete_fields = 'coordenador',
-    search_fields = 'nome_curso',
+    list_display = ("id" ,"nome_curso",)
+    search_fields = ("nome_curso",)
+    autocomplete_fields = ("coordenador",)
 
 
 @admin.register(Reservas)
 class AdminReservas(admin.ModelAdmin):
-    ...
+    list_display = ("id" ,"nome_reserva", "sala", "data_reserva", "horario_inicio", "horario_final", "coordenador")
+    list_filter = ("data_reserva", "sala", "coordenador")
+    search_fields = ("nome_reserva", "coordenador__username", "sala__nome_sala")
+    autocomplete_fields = ("bloco", "sala", "coordenador", "curso", "parent")
+    fieldsets = (
+        ("Detalhes da Reserva", {"fields": ("nome_reserva", "bloco", "sala", "data_reserva", "horario_inicio", "horario_final")}),
+        ("Informações Adicionais", {"fields": ("coordenador", "motivo_reserva", "curso")}),
+        ("Recorrência", {"fields": ("recorrencia", "quantidade_recorrencia", "parent")}),
+    )
